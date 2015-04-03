d8ed47469613280e3bd30d0da6de4a9efe1285f0
Zeki <zeki-san@hotmail.com>
2015-04-03 00:09:50 -0300

    Second commit.

--- app/src/main/java/com/example/ezequiel/crazyeights/TitleView.java
+++ app/src/main/java/com/example/ezequiel/crazyeights/TitleView.java
@@ -1,6 +1,10 @@
 package com.example.ezequiel.crazyeights;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -8,8 +12,33 @@ import android.view.View;
  */
 public class TitleView extends View{
 
+    private Bitmap titleGraphic;
+
     public TitleView(Context context){
         super(context);
+        titleGraphic = BitmapFactory.decodeResource(getResources(),R.drawable.title_graphic);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        canvas.drawBitmap(titleGraphic, 0, 0, null);
+    }
+
+    public boolean onTouchEvent(MotionEvent event){
+        int eventaction = event.getAction();
+        int X = (int)event.getX();
+        int Y = (int)event.getY();
+
+        switch (eventaction){
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        invalidate();
+        return true;
     }
 
 }
