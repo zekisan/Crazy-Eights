de9e346ea07c9850458fd5af68b5bb0eeb25cebf
Zeki <zeki-san@hotmail.com>
2015-04-02 23:27:49 -0300

    Initial commit

---
+++ app/src/main/java/com/example/ezequiel/crazyeights/TitleView.java
@@ -0,0 +1,15 @@
+package com.example.ezequiel.crazyeights;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by ezequiel on 02/04/15.
+ */
+public class TitleView extends View{
+
+    public TitleView(Context context){
+        super(context);
+    }
+
+}
